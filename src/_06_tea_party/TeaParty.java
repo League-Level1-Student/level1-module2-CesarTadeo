26e04bd429990bc70fc544ad6ace9e4c87b7d5d9
CesarTadeo <43711012+CesarTadeo@users.noreply.github.com>
2021-03-08 18:37:10 -0800

    teaparty

--- src/_06_tea_party/TeaParty.java
+++ src/_06_tea_party/TeaParty.java
@@ -5,20 +5,20 @@ public class TeaParty {
         
     if (isWoman == true) {
 		if (isKnighted == true) {
-			return "Hello Lady" + name;
+			return "Hello Lady " + name;
 		}
 		else {
-			return "Hello Ms." + name;
+			return "Hello Ms. " + name;
 		}
 
 	}
     
     else {
     	if (isKnighted == true) {
-			return "Hello Sir" + name;
+			return "Hello Sir " + name;
 		}
 		else {
-			return "Hello Mr." + name;
+			return "Hello Mr. " + name;
 	}
     
 	}
