e8c441a3deb800be6df6fe21417351a240e29570
CesarTadeo <43711012+CesarTadeo@users.noreply.github.com>
2021-03-08 18:34:10 -0800

    minion, teaparty

---
+++ src/_06_tea_party/TeaParty.java
@@ -0,0 +1,30 @@
+package _06_tea_party;
+
+public class TeaParty {
+    public String welcome(String name, boolean isWoman, boolean isKnighted) {
+        
+    if (isWoman == true) {
+		if (isKnighted == true) {
+			return "Hello Lady" + name;
+		}
+		else {
+			return "Hello Ms." + name;
+		}
+
+	}
+    
+    else {
+    	if (isKnighted == true) {
+			return "Hello Sir" + name;
+		}
+		else {
+			return "Hello Mr." + name;
+	}
+    
+	}
+}
+}
+    	
+    
+
+
